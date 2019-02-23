d8ca6502a785afe0e77dc43cb009dbf8e105da2e
Muhammed Kara <47905966+svndevelopment@users.noreply.github.com>
2019-02-23 01:41:27 +0100

    1.0.1.0

---
+++ Class/SavableClass.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using SVN.IO.Helpers;
+using SVN.Reflection;
+using SVN.Reflection.Helpers;
+
+namespace SVN.IO.Class
+{
+    public class SavableClass
+    {
+        private static JsonSerializerSettings Settings
+        {
+            get => new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Include,
+                ReferenceLoopHandling = ReferenceLoopHandling.Serialize,
+            };
+        }
+
+        private static string Extension
+        {
+            get => ".txt";
+        }
+
+        private static string FilePath
+        {
+            get
+            {
+                var path = System.IO.Path.Combine(Path.ApplicationData, Assembly.GetCallingAssemblyName(3));
+
+                if (!System.IO.Directory.Exists(path))
+                {
+                    System.IO.Directory.CreateDirectory(path);
+                }
+                
+                return System.IO.Path.Combine(path, typeof(SavableClass).GetClassName() + SavableClass.Extension);
+            }
+        }
+
+        protected SavableClass()
+        {
+        }
+
+        public static T ReadFromFile<T>()
+        {
+            var json = System.IO.File.ReadAllText(SavableClass.FilePath);
+            return JsonConvert.DeserializeObject<T>(json, SavableClass.Settings);
+        }
+
+        public void WriteToFile()
+        {
+            var json = JsonConvert.SerializeObject(this, SavableClass.Settings);
+            System.IO.File.WriteAllText(SavableClass.FilePath, json);
+        }
+    }
+}
---
+++ Helpers/Path.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SVN.IO.Helpers
+{
+    public static class Path
+    {
+        public static string ApplicationData
+        {
+            get => Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        }
+    }
+}
